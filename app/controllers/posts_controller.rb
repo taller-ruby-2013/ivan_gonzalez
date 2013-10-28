46ff59bcd7a78f1454d7ac5821805558f496705a
ivan gonzalez <ivangolo@gonzalez.ivan235@gmail.com>
2013-10-28 18:54:48 -0300

    hicimos la db y el modelo de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,24 @@ class PostsController < ApplicationController
 	def new 
 	end
 
-	def create
-		render text: params[:post].inspect
+	def index
+		@posts = Post.all
 	end
+
+	def create
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :text)
+			
+		end
 end
