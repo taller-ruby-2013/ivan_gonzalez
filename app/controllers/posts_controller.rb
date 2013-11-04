11558230f8cf9c95efe5554ec694927574f1980c
ivan gonzalez <ivangolo@gonzalez.ivan235@gmail.com>
2013-11-04 18:51:34 -0300

    CRUD de posts terminado

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	def new 
+		@post = Post.new()
 	end
 
 	def index
@@ -7,14 +8,37 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.new(post_params)
-
-		@post.save
-		redirect_to @post
+		@post = Post.new(params[:post].permit(:title, :text))
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+	  	@post = Post.find(params[:id])
+	 
+	 	if @post.update(params[:post].permit(:title, :text))
+	   		redirect_to @post
+	  	else
+	    	render 'edit'
+	  	end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to	posts_path
 	end
 
 	private
