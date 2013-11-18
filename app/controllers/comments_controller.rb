4bd696dba43835157d29b4d34ff4e4a29c0720ab
ivan gonzalez <ivangolo@gonzalez.ivan235@gmail.com>
2013-11-18 18:50:47 -0300

    Termino de la clase 6, vistas parciales comentarios en posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,12 @@ class CommentsController < ApplicationController
 		@post.comments.create(params[:comment].permit(:commenter, :body))
 		redirect_to post_path(@post)
 	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+	end
+
 end
